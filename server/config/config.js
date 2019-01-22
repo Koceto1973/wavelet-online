385b5bf102ce46153ae7007016e4071931ebab49
TeamK <koceto1973@yahoo.ca>
2019-01-22 17:33:22 +0200

    eslint/ airbnb in, code refactor

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,6 @@ const maintenance = process.env.maintenance || true; // site maintenance?
 const port = process.env.PORT || 3000;
 
 exports.configs = {
-                    maintenance,
-                    port
-                  }
+  maintenance,
+  port,
+};
--- server/server.js
+++ server/server.js
@@ -1,27 +1,30 @@
-const {configs} = require('./config/config'); // maintenance, port
-
 const path = require('path');
 
 const express = require('express');
-const app = express(); 
+
+const app = express();
 
 const hbs = require('hbs');
-hbs.registerPartials(__dirname + '/public/partials');
+const { configs } = require('./config/config'); // maintenance, port
+
+hbs.registerPartials(path.join(__dirname, '/public/partials'));
 app.set('view engine', 'hbs');
 
-app.use(express.static(path.join(__dirname ,'../public/css'))); 
-app.use(express.static(path.join(__dirname ,'../public/img')));
+app.use(express.static(path.join(__dirname, '../public/css')));
+app.use(express.static(path.join(__dirname, '../public/img')));
 
-if (configs.maintenance) { 
-  app.use((req, res, next) => {   // site under maintenance
-    res.sendFile(path.join(__dirname ,'../public/index_maintenance.html'))  // no next() here, app should block  
-  }); 
+if (configs.maintenance) {
+  app.use((req, res) => { // site under maintenance
+    res.sendFile(path.join(__dirname, '../public/index_maintenance.html')); // no next() here, app should block
+  });
 }
+
+// known routes
 
 // unknown route
 
 // activity logger
 
 app.listen(configs.port, () => {
-    console.log(`Server is up and running on port ${configs.port}!`); 
+  console.log(`Server is up and running on port ${configs.port}!`);
 }); // https://limitless-bayou-85712.herokuapp.com/
