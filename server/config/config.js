0e9920ee6399222e5fd0e244f7e07f1736d00810
TeamK <koceto1973@yahoo.ca>
2019-02-01 09:21:16 +0200

    debug remote

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,14 @@
-const key = require('./config.json');
+const env = process.env.NODE_ENV || 'development'; // production takes precedence if any
+
+let key = '';
+
+if (env === 'development') { // see package json
+  key = require('./config.json');
+}
 
 const tuneKey = process.env.tuneKey ? process.env.tuneKey : key.tuneKey;
 
 const maintenance = process.env.maintenance ? process.env.maintenance : false;
-
 
 const port = process.env.PORT || 3000;
 
