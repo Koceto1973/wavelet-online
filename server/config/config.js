49f780072544b1a0a87a6d82405cb448bb75ca05
TeamK <koceto1973@yahoo.ca>
2019-02-02 06:30:13 +0200

    heroku debug

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,9 @@
 const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'development'; // production takes precedence if any
 
 let maintenance = true;
 
-if (process.env.NODE_ENV === 'production') {
+if (env !== 'development') {
   maintenance = process.env.maintenance;
 } else {
   maintenance = false;
