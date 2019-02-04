056a039c58d065be2807851a1998314528258684
TeamK <koceto1973@yahoo.ca>
2019-02-04 15:16:39 +0200

    resume route vertical buttons specific listeners in

---
+++ public/scripts/helpF.js
@@ -0,0 +1,7 @@
+export const addButtons = (arr) => {
+  console.log(arr);
+};
+
+export const addSomethingElse = (arr) => {
+  console.log(arr);
+};
--- public/scripts/script_r.js
+++ public/scripts/script_r.js
@@ -1,6 +1,23 @@
+const addButtons = (arr) => {
+  arr.forEach((item) => {
+    document.getElementById('h').insertAdjacentHTML('beforeend', `<li><button>${item}</button></li>`);
+  });
+};
+
+const init = () => {
+  // clear all h-buttons, text, picture and joke
+  document.getElementById('h').innerHTML = '';
+  document.getElementById('text').textContent = '';
+  document.getElementById('joke').textContent = '';
+  // document.querySelector('picture').innerHTML = '';
+};
+
+// initialization
+init();
+
 // get all buttons
 document.querySelectorAll('button').forEach((b) => {
-  // add listener to each v-button
+  // add init listener to each v-button
   if (b.parentElement.parentElement.id !== 'h') {
     b.addEventListener('click', () => {
       // format all to default
@@ -9,12 +26,27 @@ document.querySelectorAll('button').forEach((b) => {
       });
       // format clicked to bolded border
       b.style.border = '3px solid mediumseagreen';
-      // clear all h-buttons, text, picture and joke
-      document.getElementById('h').innerHTML = '';
-      document.getElementById('text').textContent = '';
-      document.getElementById('joke').textContent = '';
-      document.querySelector('picture').innerHTML = '';
+      init();
     });
   }
 });
 
+// add specific listener to each v-button
+document.getElementById('Bio').addEventListener('click', () => {
+  addButtons(['PersonalDetails', 'Background', 'CurrentStatus']);
+});
+document.getElementById('Programming').addEventListener('click', () => {
+  addButtons(['C / C++', 'C#', 'Swift', 'HTML/CSS', 'JS / Node.js']);
+});
+document.getElementById('Science').addEventListener('click', () => {
+  addButtons(['Math', 'Phisycs', 'CS']);
+});
+document.getElementById('Motivation').addEventListener('click', () => {
+  addButtons(['Roots', 'Resources', 'Reality']);
+});
+document.getElementById('Objectives').addEventListener('click', () => {
+  addButtons(['Short Term', 'Mid Term', 'Long Term']);
+});
+document.getElementById('Settings').addEventListener('click', () => {
+  addButtons(['colors', 'font', 'effects']);
+});
