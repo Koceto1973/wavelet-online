d7786a402f65b9bfea8c4564c5d4e9fee35a4db8
TeamK <koceto1973@yahoo.ca>
2019-02-04 18:23:51 +0200

    resume route horizontal buttons specific listeners in

--- public/scripts/script_r.js
+++ public/scripts/script_r.js
@@ -33,15 +33,15 @@ document.querySelectorAll('button').forEach((b) => {
 
 // v-buttons specific listeners
 document.getElementById('Bio').addEventListener('click', () => {
-  addButtons(['PersonalDetails', 'Background', 'CurrentStatus']);
+  addButtons(['ID', 'Background', 'Current']);
   document.getElementById('joke').textContent = 'The old man and the sea :)';
 });
 document.getElementById('Programming').addEventListener('click', () => {
-  addButtons(['C / C++', 'C#', 'Swift', 'HTML/CSS', 'JS / Node.js']);
+  addButtons(['C/C++', 'C#', 'Swift', 'HTML/CSS', 'JS/Node.js']);
   document.getElementById('joke').textContent = 'Between Siri and Alexa :)';
 });
 document.getElementById('Science').addEventListener('click', () => {
-  addButtons(['Math', 'Phisycs', 'CS']);
+  addButtons(['Math', 'Physics', 'CS']);
   document.getElementById('joke').textContent = 'Just a curiousity intoxication :)';
 });
 document.getElementById('Motivation').addEventListener('click', () => {
@@ -49,7 +49,7 @@ document.getElementById('Motivation').addEventListener('click', () => {
   document.getElementById('joke').textContent = 'Harder than a rock :)';
 });
 document.getElementById('Objectives').addEventListener('click', () => {
-  addButtons(['Short Term', 'Mid Term', 'Long Term']);
+  addButtons(['ShortTerm', 'MidTerm', 'LongTerm']);
   document.getElementById('joke').textContent = 'Gambling with the Gods :)';
 });
 document.getElementById('Settings').addEventListener('click', () => {
@@ -69,5 +69,69 @@ document.getElementById('sectionHeader').addEventListener('click', (event) => {
     // format the target button correctly
     document.getElementById(id).style.border = '3px solid mediumseagreen';
     // switch the target request
+    switch (id) {
+      case 'ID':
+        console.log('ID');
+        break;
+      case 'Background':
+        console.log('Background');
+        break;
+      case 'Current':
+        console.log('Current');
+        break;
+      case 'C/C++':
+        console.log('C/C++');
+        break;
+      case 'C#':
+        console.log('C#');
+        break;
+      case 'Swift':
+        console.log('Swift');
+        break;
+      case 'HTML/CSS':
+        console.log('HTML/CSS');
+        break;
+      case 'JS/Node.js':
+        console.log('JS/Node.js');
+        break;
+      case 'Math':
+        console.log('Math');
+        break;
+      case 'Physics':
+        console.log('Physics');
+        break;
+      case 'CS':
+        console.log('CS');
+        break;
+      case 'Roots':
+        console.log('Roots');
+        break;
+      case 'Resources':
+        console.log('Resources');
+        break;
+      case 'Reality':
+        console.log('Reality');
+        break;
+      case 'ShortTerm':
+        console.log('ShortTerm');
+        break;
+      case 'MidTerm':
+        console.log('MidTerm');
+        break;
+      case 'LongTerm':
+        console.log('LongTerm');
+        break;
+      case 'colors':
+        console.log('colors');
+        break;
+      case 'font':
+        console.log('font');
+        break;
+      case 'effects':
+        console.log('effects');
+        break;
+
+      default:
+    }
   }
 });
