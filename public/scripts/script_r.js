edfe2edd8e850177d2779ee9dc83f4e196a414e3
TeamK <koceto1973@yahoo.ca>
2019-02-04 17:42:27 +0200

    resume route horizontal buttons init listeners in

--- public/scripts/script_r.js
+++ public/scripts/script_r.js
@@ -1,6 +1,6 @@
 const addButtons = (arr) => {
   arr.forEach((item) => {
-    document.getElementById('h').insertAdjacentHTML('beforeend', `<li><button>${item}</button></li>`);
+    document.getElementById('h').insertAdjacentHTML('beforeend', `<li><button id=${item}>${item}</button></li>`);
   });
 };
 
@@ -15,7 +15,7 @@ const init = () => {
 // initialization
 init();
 
-// get all buttons
+// v-buttons init listeners
 document.querySelectorAll('button').forEach((b) => {
   // add init listener to each v-button
   if (b.parentElement.parentElement.id !== 'h') {
@@ -31,22 +31,43 @@ document.querySelectorAll('button').forEach((b) => {
   }
 });
 
-// add specific listener to each v-button
+// v-buttons specific listeners
 document.getElementById('Bio').addEventListener('click', () => {
   addButtons(['PersonalDetails', 'Background', 'CurrentStatus']);
+  document.getElementById('joke').textContent = 'The old man and the sea :)';
 });
 document.getElementById('Programming').addEventListener('click', () => {
   addButtons(['C / C++', 'C#', 'Swift', 'HTML/CSS', 'JS / Node.js']);
+  document.getElementById('joke').textContent = 'Between Siri and Alexa :)';
 });
 document.getElementById('Science').addEventListener('click', () => {
   addButtons(['Math', 'Phisycs', 'CS']);
+  document.getElementById('joke').textContent = 'Just a curiousity intoxication :)';
 });
 document.getElementById('Motivation').addEventListener('click', () => {
   addButtons(['Roots', 'Resources', 'Reality']);
+  document.getElementById('joke').textContent = 'Harder than a rock :)';
 });
 document.getElementById('Objectives').addEventListener('click', () => {
   addButtons(['Short Term', 'Mid Term', 'Long Term']);
+  document.getElementById('joke').textContent = 'Gambling with the Gods :)';
 });
 document.getElementById('Settings').addEventListener('click', () => {
   addButtons(['colors', 'font', 'effects']);
+  document.getElementById('joke').textContent = '... without the one, you actually need :)';
+});
+
+// h-buttons init listeners
+document.getElementById('sectionHeader').addEventListener('click', (event) => {
+  const { id } = event.target;
+  if (id !== 'h') { // button was pressed
+    // format all h-buttons
+    const vButtons = document.getElementById('h').children;
+    Array.prototype.forEach.call(vButtons, (child) => {
+      child.firstChild.style.border = '1px solid mediumseagreen';
+    });
+    // format the target button correctly
+    document.getElementById(id).style.border = '3px solid mediumseagreen';
+    // switch the target request
+  }
 });
