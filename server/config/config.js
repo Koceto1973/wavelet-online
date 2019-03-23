c8180368f359dd8b32718d83ba807ea5d55ae217
TeamK <koceto1973@yahoo.ca>
2019-03-23 17:53:15 +0200

    home router ip discovery service debug

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,13 @@ const env = process.env.NODE_ENV || 'development'; // production takes precedenc
 const maintenance = false; // local equivalent of process.env.maintenance on remote
 
 let userKey = '';
+let globalIPv4 = '';
 if (env === 'development') {
   userKey = require('./config.json').userKey;
+  globalIPv4 = require('./config.json').globalIPv4;
 } else {
   userKey = process.env.userKey;
+  userKey = process.env.globalIPv4;
 }
 
 exports.configs = {
@@ -15,4 +18,5 @@ exports.configs = {
   env,
   maintenance,
   userKey,
+  globalIPv4,
 };
